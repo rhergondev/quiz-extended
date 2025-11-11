48532f42db198014300c1ef469f66ce1a972df51
Raul Hernandez <rhergondev@gmail.com>
2025-11-11 15:55:43 +0100

    fixed courses list

--- admin/react-app/build/index.asset.php
+++ admin/react-app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '5d888d987ae72848c8b0');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '946804de3652818a98a6');
