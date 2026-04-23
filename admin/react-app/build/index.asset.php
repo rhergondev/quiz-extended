030507cadf964070231e2d47c1165aaec0a53410
Raul Hernandez <rhergondev@gmail.com>
2026-04-23 19:54:22 +0200

    auto zoom fix

--- admin/react-app/build/index.asset.php
+++ admin/react-app/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => 'a58c822b556173a38601');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => '2290c59de8c04cb18065');
